1c0500e4679ee32d55e465eb73e5fc45f7bcb5e9
Gabriela Mendes <gabriela_m_ferreira@outlook.com>
2026-08-05 08:54:09 -0300

    feat: publish Soma e Produto de Dois Números (Java)

---
+++ logic/java/soma-e-produto-de-dois-numeros/solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // TODO: Ler o primeiro número inteiro
+        // TODO: completar 
+        int primeiroNumero = scanner.nextInt();
+
+        // TODO: Ler o segundo número inteiro
+        // TODO: completar 
+        int segundoNumero = scanner.nextInt();
+
+        // TODO: Calcular a soma
+        // TODO: completar 
+        int soma = primeiroNumero + segundoNumero;
+
+        // TODO: Calcular o produto
+        // TODO: completar 
+        int produto = primeiroNumero * segundoNumero;
+
+        // TODO: Imprimir a soma
+        System.out.println(soma);
+
+        // TODO: Imprimir o produto
+
+        System.out.println(produto);
+    }
+}
