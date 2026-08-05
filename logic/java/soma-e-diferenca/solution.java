120e6730a6d0a4f9c3ef8fb27ee91d7167f57e3d
Gabriela Mendes <gabriela_m_ferreira@outlook.com>
2026-08-05 08:56:50 -0300

    feat: publish Soma e Diferença (Java)

---
+++ logic/java/soma-e-diferenca/solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // TODO: Ler o primeiro número inteiro
+        int num1 = scanner.nextInt();
+
+        // TODO: Ler o segundo número inteiro
+        int num2 = scanner.nextInt();
+
+        // TODO: Calcular a soma
+        int soma = num1 + num2; // substitua com o cálculo da soma
+
+        // TODO: Calcular a diferença
+        int diferenca = num1 - num2; // substitua com o cálculo da diferença
+
+        // Exibir resultado no formato especificado
+        System.out.println("Soma: " + soma + ", Diferença: " + diferenca);
+    }
+}
